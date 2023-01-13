6bc4cb05fb981deaa647e9b23b5cf837588d57b5
jonatan <jonatan.gron.code@hotmail.com>
2023-01-13 23:07:06 +0100

    Initial Commit

---
+++ Firebase-Shoppinglist/Firebase_ShoppinglistApp.swift
@@ -0,0 +1,17 @@
+//
+//  Firebase_ShoppinglistApp.swift
+//  Firebase-Shoppinglist
+//
+//  Created by Admin on 2023-01-13.
+//
+
+import SwiftUI
+
+@main
+struct Firebase_ShoppinglistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
